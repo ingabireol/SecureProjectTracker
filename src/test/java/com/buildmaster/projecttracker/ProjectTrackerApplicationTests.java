217e79ced4b58c39271a2942b9b39f717071d006
ingabireol <ingabire069@gmail.com>
2025-06-06 07:29:25 +0200

    Initual commit

---
+++ src/test/java/com/buildmaster/projecttracker/ProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmaster.projecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
